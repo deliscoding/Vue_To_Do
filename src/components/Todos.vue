dd5d379621a52de37caf5caeeffbab7c8bcc8ae0
Dan Elis <danelis19@gmail.com>
2019-02-07 12:09:57 +0100

    added delete btn, method, emit works

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,7 +3,7 @@
     <!-- <h1>Todos</h1> -->
     <div v-bind:key="todo.id" v-for="todo in todos">
       <!-- <h3>{{todo.title}}</h3> -->
-      <TodoItem v-bind:todo="todo"/>
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
     </div>
   </div>
 </template>
