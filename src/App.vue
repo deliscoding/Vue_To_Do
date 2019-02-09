7f481511f92327c0b187bb6580827fbcec6d7d3d
Dan Elis <danelis19@gmail.com>
2019-02-09 13:11:56 +0100

    created Header, AddToDo & styling

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
+    <Header/>
+    <AddToDo/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
+import AddToDo from "./components/AddToDo";
 
 export default {
   name: "app",
   components: {
-    Todos
+    Header,
+    Todos,
+    AddToDo
   },
   data() {
     return {
